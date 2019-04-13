b0b53b980c4f9e1552483f5b04d7a8bb339a918d
alpecoro <48429869+alpecoro@users.noreply.github.com>
2019-04-13 17:40:27 +0300

    upd

--- task4/task5.js
+++ task4/task5.js
@@ -3,7 +3,7 @@ function string (str1, str2) {
     var newStr1 = str1.toLowerCase();
     var newStr2 = str2.toLowerCase();
 
-    if (newStr1.includes(newStr2) != false) {
+    if (newStr1.indexOf(newStr2) != -1) {
         return true
     } else {
         return false
